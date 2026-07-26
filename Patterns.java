29b6c8232bfc4dfddb240e444bf927d94eecd6f4
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-07-26 21:25:50 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,45 @@
+// 
+ublic class Patterns {
+public static void main(String[] args) {
+
+
+
+
+
+
+                        //    // move to next line
+                        //      System. out.println(); 
+                        // int n=5;
+                        //   for(int row=1; row<=n; row++){
+                        //     // for each row -> variable columns
+                        //     if(row==1 || row==2 || row==n){
+                        //         for(int col=1; col<=row; col++){
+                        //             System.out.print("*");
+                        //         }
+                        //     }
+                        //     else{
+                        //         // midddle rows
+                        //         //|1*
+                        //         System.out.print("*");
+                        //         // row-2  sapces  
+                        //         for(int col=1; col<=row-2; col++){
+                        //             System.out.print(" ");
+                        //         }
+                        //         //1*
+                        //         System.out.print("*");
+                        //      }
+                        //    out.println(); 
+                        //   }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         }
+               }
