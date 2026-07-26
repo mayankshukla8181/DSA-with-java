e8c239cc52d47aa6adbf7b72dca2712814a34cca
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-07-26 21:03:35 +0530

    Add files via upload

---
+++ StringsBasics.java
@@ -0,0 +1,64 @@
+  public class StringsBasics{
+   public static void main(String args[]){
+//int num=7687
+    // String str=String.valueOf(num); 
+//     Strin g name="mayank";
+// System.out.println(name.startsWith("mayank"));
+String input="my name is mayank";
+Stringh[] words=input.split(" ");
+for(String str:words){
+    System.out.println(str);}
+    // String firstName="Mayank";
+    // String lastName="Shula";
+    // System.out.println(firstName + " " + lastName);
+    // System.out.println(firstName.length());
+    // System.out.println(firstName.charAt(3));
+    //  String name="shukla"; 
+    //  name[0]=b
+    //  name="mayank";
+    // System.out.println(name);
+ //empity->length=0
+ // blank->empty or spaces hai string 
+//  
+// Scanner sc=new Scanner(System.in);
+//     System.out.println("Provide the string content:");
+//     String str=sc.nextLine();
+//     System.out.println("value :"+str);
+    // String name1="mayank";
+    // String name2="mayank";
+    // if(name1==name2)
+    // System.out.println("Both are equal");
+    
+    // }
+    //    else{
+    //     System.out.println("Both are not equal"); 
+    //     }
+
+// String name1="mayank";
+// String name2="mayank";
+// if(name1.equals(name2)){
+// System.out.println("Both are equal");
+
+// }
+//    else{
+//     System.out.println("Both are not equal"); 
+    // String name1="mayank";
+    // System.out.println(name1.toUpperCase());
+    // String name2="MAYANK";
+    // System.out.println(name2.toLowerCase());  
+    // String name1=" my name is mayank";
+    //  System.out.println(name1.contains("mayank") );
+    
+    
+    
+    }
+
+
+
+
+   }
+
+
+
+ 
+//  
