244a6645bfae549802ab4746b23d42f3c5cf738a
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-07-26 21:23:41 +0530

    Add files via upload

---
+++ QueueBasics.java
@@ -0,0 +1,34 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class QueueBasics{
+    public static void main(String[] args){
+        // Queue<Integer> q= new LinkedList<>();
+        // q.offer(58);
+        // q.offer(197);
+        // q.offer(9119);
+        // System.out.println(q);
+        //  System.out.println("Removing: "+ q.poll());
+        // System.out.println(q);
+        // System.out.println("peeking:"+ q.peek());
+
+        // Deque<Integer> q = new ArrayDeque<>();
+        // q.offer(8);
+        //  q.offer(97);
+        //  q.offer(119);
+        // System.out.println(q);
+        // q.pollFirst();
+        // q.pollLast();
+        // System.out.println(q);
+        // System.out.println(q.peek());
+          public class stackBasics{
+    public static void main(String[] args){
+            Deque<Integer> stack = new ArrayDeque<>();
+             stack.push(11);
+             stack.push(33);
+             stack.push(28);
+             System.out.println(satck);
+    }
+}
