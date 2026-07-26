6e69f746f8a1e48d82b1d28d55b70edeb5ee7d4d
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-07-26 21:27:22 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,109 @@
+
+public class Arrays{
+     public static void main(String args[]){
+        int arr[][];
+                   int arr[][]=new int[3][4];
+                  scanner sc=new Scanner(System.in); 
+                  
+                  for(int i=0; i<=arr.length-1; i++){
+                      for(int j=0; j<=arr[i].length-1; j++){
+                          System.out.print("Provide value for row =  " + i + " and column =" + j + ": ");
+                          arr[i][j]=sc.nextInt();
+                      }
+        // //  int arr[]= {4, 2, -5, 21, 15};
+        // //    int n= arr.length; 
+        // //      int Minvalue=arr[0];
+        // // //      compare the array element with maxvalue
+        // //      for(int i=0; i<=n-1; i++){
+        // //          if(arr[i]<Minvalue){
+        // //              Minvalue=arr[i];
+        // //          }
+        // //      }
+        // //      System.out.println("Minimum value of array: "+Minvalue);
+        // //  }
+        // //     decelration 
+        //          int[][] arr ;
+        //         //  allocation
+        //               arr=new int[3][4];
+        //         //       inti
+        //           int[][] brr= {
+        //                 {1,2},{2,3},{3,4},{4,5} 
+        //           };
+        //                 System.out.println(brr[3][0]);
+                  
+                  
+
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  }
+}
+}
+
+ 
+ 
+
+
+//        int arr[]=new int[]{10,20,30,40,50};
+//         int ans=1;
+//         int n= arr.length;
+//         for(int i=0; i<=n-1; i++){
+//             ans=ans*arr[i];
+//         }
+//         System.out.println("Multiplication of array element: "+ans);
+//      }
+// }
+        // // deceleration 
+        // int arr[];
+        // //allocation
+        // arr=new int[5];     
+        //  // initialization
+        // int brr []={10,20,30,40,50};
+        // int n=brr.length;
+        // for(int va l:brr){
+        //     System.out.println(val);
+        // }
+// int arr[]=new int[5]; 
+// Scanner sc=new Scanner(System.in);
+// int n=arr.length;
+// for(int i=0; i<=(n-1); i++ ){
+//     System.out.print("Enter the value of" +i + " index: ");
+//     arr[i]= sc.nextInt();
+// } 
+// System.out.println("you array contain: ");
+// for (int val:arr){
+//     System.out.println(val);
+//      }
+//      sc.close();
+//      }
+// }
+
+
+
+         // int lenght=brr.length;
+        // for(int index=0;index<=lenght-1;index++){
+        //     System.out.println("value of "+index+" index: "+brr[index]);
+        //     System.out.println (brr[index]);
+    // }
+    //  }
+ 
+//         System.out.println("value of 0 index: " + brr[0]);
+//         System.out.println("value of 1 index: " + brr[1]);
+//         System.out.println("value of 2 index: " + brr[2]);
+//         System.out.println("value of 3 index: " + brr[3]);
+        // System.out.println("value of 4 index: " + brr[4]);    }
+//  }
+     
