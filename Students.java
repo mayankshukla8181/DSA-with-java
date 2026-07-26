485d4666dabacb716a9a41a9917494c2d9f06da5
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-07-26 21:03:15 +0530

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,108 @@
+// public class Students {
+//     //properties\ attributes
+// private String name;
+// private int age;
+// private String address; 
+// private String course;
+// private int rollno;
+
+// //methods\ bhevaiors
+// public void displayStudentDetails(){
+//     System.out.println("Student Details: ");    
+// System.out.println("name: " + name + ", age: " + age + ", address: " + address + ", course: " + course + ", rollno: " + rollno); 
+// }
+//  main method
+// public static void main(String args[]) {
+//     Students s1=new Students();
+//     s1.name="Mayank";
+//     s1.age=22;
+//     s1.address="Amethi";
+//     s1.course="Java";
+//     s1.rollno=101;
+    
+// //     public Students A=new Students(id:102, name:"Rohit", age:23, address:"Lucknow", course:"Python", rollno:102 );
+// //    this.id=id;
+// //    this.name=name;
+// //     this.age=age;   
+// //     this.address= address;
+// //     this.course=course;
+//    System.out.println("s1.name");
+//    System.out.println("s1.age");
+//    System.out.println("s1.address");
+//    System.out.println("s1.course");
+//    System.out.println("s1.rollno");
+// }
+// public class Students {
+
+//     // Properties
+//     private String name;
+//     private int age;
+//     private String address;
+//     private String course;
+//     private int rollno;
+
+//     // Constructor
+// //     public Students(String name, int age, String address, String course, int rollno) {
+// //         this.name = name;
+// //         this.age = age;
+// //         this.address = address;
+// //         this.course = course;
+// //         this.rollno = rollno;
+// //     }
+
+// //     public static void main(String[] args) {
+
+// //         Students A = new Students("Rohit", 23, "Lucknow", "Python", 102);
+
+// //         System.out.println(A.name);
+// //         System.out.println(A.age);
+// //         System.out.println(A.address);
+// //         System.out.println(A.course);
+// //         System.out.println(A.rollno);
+//     }
+// }
+public class Students {
+
+    // Properties
+    private String name;
+    private int age;
+    private String address;
+    private String course;
+    private int rollno;
+
+    // Method
+    public void displayStudentDetails() {
+        System.out.println("Student Details:");
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Address : " + address);
+        System.out.println("Course : " + course);
+        System.out.println("Roll No : " + rollno);
+    }
+
+    // Main method
+    public class Students() {
+
+        Students s1 = new Students();
+
+        s1.name = "Mayank";
+        s1.age = 22;
+        s1.address = "Amethi";
+        s1.course = "Java";
+        s1.rollno = 101;
+
+        s1.displayStudentDetails();
+
+
+    }
+// public Students(student scrobj){
+//     Student s2= new  student s1
+//     System.out.println(s1.name);
+//     System.out.println(s1.age);
+//  System.out.println(s1.curse );
+// }
+ 
+
+
+
+}
