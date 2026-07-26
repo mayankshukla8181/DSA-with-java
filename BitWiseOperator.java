c88e31c4737dc8a9a830cba5b53205da4e743478
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-07-26 20:56:41 +0530

    Add files via upload

---
+++ BitWiseOperator.java
@@ -0,0 +1,22 @@
+public class BitWiseOperator {
+
+    public static void main(String[] args) {
+        // int a = 5;
+        // int b = 6;
+
+        // System.out.println(a & b);
+        // int n=1;
+        // for(int i=1; n<=32; i++){
+        //     n = n<<1;
+        //     System.out.println(n);
+        //     System.out.println();
+        // }
+         int n=100;
+         for(int i=1; i<=10; i++){
+            n = n>>1;
+            System.out.println(n);
+              System.out.println();
+         }
+
+    }
+}
