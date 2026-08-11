94220c160abf6b03100ea53eef08f1f0dd950199
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-08-11 22:22:28 +0530

    Time: 318 ms (5.03%), Space: 47.6 MB (8.73%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,31 +1,49 @@
 class Solution {
-    public int pivotIndex(int[] nums) {
+    public int pivotIndex(int[] arr) {
 
-        int totalSum = 0;
+//         int totalSum = 0;
 
-        // Calculate total sum
-        for (int num : nums) {
-            totalSum += num;
-        }
+//         // Calculate total sum
+//         for (int num : nums) {
+//             totalSum += num;
+//         }
 
-        int leftSum = 0;
+//         int leftSum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
+//         for (int i = 0; i < nums.length; i++) {
 
-            int rightSum = totalSum - leftSum - nums[i];
+//             int rightSum = totalSum - leftSum - nums[i];
 
-            if (leftSum == rightSum) {
-                return i;
-            }
+//             if (leftSum == rightSum) {
+//                 return i;
+//             }
 
-            leftSum += nums[i];
-        }
+//             leftSum += nums[i];
+//         }
 
-        return -1;
+//         return -1;
+//     }
+// }
+            
+          int n = arr.length ;
+
+   for(int i = 0; i <= n-1; i++){
+    int left = 0;
+    int right = 0;
+     for(int j = 0 ; j < i; j++){
+         left = left + arr[j];
+          }
+            for (int j  = i + 1; j < n; j++){
+            right = right + arr[j];
+           }
+           if(right == left){
+            return i;
+           }
+           
+   }
+   return -1; 
+   }
     }
-}
-
-
 
 
 
