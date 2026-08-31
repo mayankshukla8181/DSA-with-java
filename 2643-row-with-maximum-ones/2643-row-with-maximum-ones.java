d3bf03ee46918fd94943c4d529dfad290ff23914
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-08-31 21:02:31 +0530

    Time: 5 ms (15.19%), Space: 48 MB (88.91%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
@@ -4,19 +4,19 @@ class Solution {
 
         int maxOnes = 0;
         int maxRowIndex = 0;
-
+            // Ye rows ko traverse karega.
         for (int i = 0; i < mat.length; i++) {
 
             int count = 0;
-
+               //Ye current row ke columns ko traverse karega.
             for (int j = 0; j < mat[i].length; j++) {
-
+               // i-th row aur j-th column ki value
                 if (mat[i][j] == 1) {
                     count++;
                 }
             }
 
-            // Use >, not >=, to keep the smallest row index in case of tie
+            /*Agar current row mein ones ki quantity ab tak ke maximum se zyada hai, to maximum ko update karo.*/
             if (count > maxOnes) {
                 maxOnes = count;
                 maxRowIndex = i;
