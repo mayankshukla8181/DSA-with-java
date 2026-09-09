3aedd42fbe8342d77b4fadca74d5851a5b59c3cd
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-09-09 21:34:51 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+    static int fact(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        int ans=fact(5);
+        System.out.println(ans);
+    }
+}
