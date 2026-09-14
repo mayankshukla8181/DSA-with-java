f04f281c0d25b44bf6da5ad3acd489c94a719b5b
mayankshukla8181 <mayanksshukla8181@gmail.com>
2026-09-14 21:20:06 +0530

    Add files via upload

---
+++ reculsiveSeries.java
@@ -0,0 +1,20 @@
+public class reculsiveSeries {
+    static int series(int n){
+    //     if(n==1){
+    //         return 1;
+    //     }
+    //     else{
+    //         return n+series(n-1);
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     int ans=series(5);
+    //     System.out.println(ans);
+    if(n==0){
+        return;
+    }
+    System.out.println("hello");
+    printSeries(n-1);
+    }
+    // }
+}
